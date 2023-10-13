8f2752fe887ce05977eaa15c995901800535900d
Alan B <death1027@outlook.com>
2023-10-13 15:49:51 -0600

    build: :construction_worker: add deploy file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,36 +1,37 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'Analytics API',
-			social: {
-				github: 'https://github.com/BloodBoy21/analyticAPI',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'API Reference',
-					items: [
-						{
-							label: 'Analytics',
-							link: '/endpoints/analytics/',
-						},
-						{
-							label: 'Documents',
-							link: '/endpoints/documents/',
-						}
-					]
-				},
-			],
-		}),
-	],
+  site: "https://bloodboy21.github.io",
+  integrations: [
+    starlight({
+      title: "Analytics API",
+      social: {
+        github: "https://github.com/BloodBoy21/analyticAPI",
+      },
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", link: "/guides/example/" },
+          ],
+        },
+        {
+          label: "API Reference",
+          items: [
+            {
+              label: "Analytics",
+              link: "/endpoints/analytics/",
+            },
+            {
+              label: "Documents",
+              link: "/endpoints/documents/",
+            },
+          ],
+        },
+      ],
+    }),
+  ],
 });
